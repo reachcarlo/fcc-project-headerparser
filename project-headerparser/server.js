d65f6a5e3a10e32cddcc11a35c2f56906c245c13
reachcarlo <44904724+reachcarlo@users.noreply.github.com>
2019-07-26 14:43:59 -0700

    project-headerparser-seed

---
+++ project-headerparser/server.js
@@ -0,0 +1,36 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require("express");
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/whoami", function(req, res) {
+  var ipaddress = req.headers["x-forwarded-for"].split(",").shift();
+  var language = req.headers["accept-language"];
+  var software = req.headers["user-agent"];
+  res.json({ ipaddress: ipaddress, language: language, software: software });
+});
+
+// your first API endpoint...
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
